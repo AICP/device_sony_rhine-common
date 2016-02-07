bad027672be1f0fc7c33420604866773d26d5d56
Erik Castricum <erikcas1972@gmail.com>
2016-02-07 22:45:21 +0100

    include config_omni.mk in device/sony/common
    we will include config.mk there
    
    Change-Id: I60e030f895880be28b97fd0a42459547cef9dd84
    Signed-off-by: Erik Castricum <erikcas1972@gmail.com>

--- platform.mk
+++ platform.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, device/sony/common/common.mk)
+$(call inherit-product, device/sony/common/common_omni.mk)
 
 SOMC_PLATFORM := rhine
 
