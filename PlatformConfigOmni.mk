8ad26e6e93222c577c2643b22f4de77e67209274
Erik Castricum <erikcas1972@gmail.com>
2016-02-08 21:06:32 +0100

    rhine: move TWRP configs to common
    Change-Id: I8f7a053c2f27fc790dddb51a596b7733788adfda
    Signed-off-by: Erik Castricum <erikcas1972@gmail.com>

--- PlatformConfigOmni.mk
+++ PlatformConfigOmni.mk
@@ -14,26 +14,8 @@
 
 include device/sony/rhine-common/PlatformConfig.mk
 
-# TWRP
-BOARD_HAS_NO_SELECT_BUTTON := true
-
 # TWRP flags
 TW_THEME := portrait_hdpi
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TW_HAS_NO_RECOVERY_PARTITION := true
-TW_FLASH_FROM_STORAGE := true
-TW_EXTERNAL_STORAGE_PATH := "/external_sd"
-TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 TW_DEFAULT_EXTERNAL_STORAGE := true
-TW_INCLUDE_JB_CRYPTO := false
-TW_INCLUDE_L_CRYPTO := true
-TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
-TW_MAX_BRIGHTNESS := 4095
 TW_NO_USB_STORAGE := true
-TW_CRYPTO_FS_TYPE := "ext4"
-TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/msm_sdcc.1/by-name/userdata"
-TW_CRYPTO_MNT_POINT := "/data"
-TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,barrier=1,noauto_da_alloc,discard"
-TW_CRYPTO_FS_FLAGS := "0x00000406"
-TW_CRYPTO_KEY_LOC := "footer"
-TW_INCLUDE_FUSE_EXFAT := true
