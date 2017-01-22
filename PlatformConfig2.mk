89b86496831c171bb296acca0f273a8097ddecca
SpiritCroc <spiritcroc@gmail.com>
2017-01-22 10:32:47 +0100

    Revert "Rhine: move custom display HAL to platform"
    This reverts commit a35804bab27160cb257183e07bd34e84f44085fb.
    
    Change-Id: Icb3cda4fadc15c94d96337530602dbd1ba7205d7

--- PlatformConfig2.mk
+++ PlatformConfig2.mk
@@ -25,9 +25,5 @@ TARGET_KERNEL_SOURCE := kernel/sony/msm
 # Nfc
 TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
 
-# Display HAL
-SONY_AOSP_DISPLAY := true
-$(call project-set-path,qcom-display,hardware/sony/display/msm8994)
-
 # RIL class override
 BOARD_RIL_CLASS := ../../../$(PLATFORM_COMMON_PATH)/ril/
