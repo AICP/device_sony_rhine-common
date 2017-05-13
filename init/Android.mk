CyanogenMod/android_device_sony_shinano-common

Build init as C++
Change-Id: Ie6212aa11d542a3b42a6be0ac21e48d1b400c177

init/Android.mk
@@ -6,7 +6,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall
-LOCAL_SRC_FILES := init_shinano.c
+LOCAL_SRC_FILES := init_shinano.cpp
 LOCAL_MODULE := libinit_shinano
 include $(BUILD_STATIC_LIBRARY)
 

AICP/device_sony_rhine-common

Signed-off-by: Chirayu Desai <chirayudesai1@gmail.com>

init/Android.mk
@@ -6,7 +6,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall
-LOCAL_SRC_FILES := init_rhine.c
+LOCAL_SRC_FILES := init_rhine.cpp
 LOCAL_MODULE := libinit_rhine
 include $(BUILD_STATIC_LIBRARY)
 