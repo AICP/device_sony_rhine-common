643441c236f0a5323e6677cf97021ebc9f615704
Ondrej Hlavac <hlavaco@gmail.com>
2017-06-23 17:40:11 +0200

    rhine-common: camera init: Add rhine's specific shims
    Change-Id: Ide5ed79735146658b9685f79738ccc91fa9fe766

--- rhine.mk
+++ rhine.mk
@@ -43,7 +43,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/fstab.qcom:root/fstab.qcom \
     $(COMMON_PATH)/rootdir/init.qcom.rc:root/init.qcom.rc \
-    $(COMMON_PATH)/rootdir/init.shims.rc:root/init.shims.rc \
+    $(COMMON_PATH)/rootdir/init.camera.rc:root/init.camera.rc \
     $(COMMON_PATH)/rootdir/system/etc/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
     $(COMMON_PATH)/rootdir/ueventd.qcom.rc:root/ueventd.qcom.rc
 
