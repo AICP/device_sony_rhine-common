f6688ee2b8be1d37bb45bd562f8853f52571d9a8
Max Weffers <rcstar6696@gmail.com>
2017-12-30 21:55:43 +0100

    rhine-common: Update for early-mounted system partition
    Change-Id: If3cfd2c489e4c73673d1cf71176eb12baee19d12

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -79,7 +79,7 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
 # Recovery
-TARGET_RECOVERY_FSTAB := device/sony/rhine-common/rootdir/fstab.qcom
+TARGET_RECOVERY_FSTAB := device/sony/rhine-common/rootdir/fstab.full
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
