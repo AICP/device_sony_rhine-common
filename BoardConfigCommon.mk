6ebc849b6dc59daa818172b657e1d0273e8420b4
Max Weffers <rcstar6696@gmail.com>
2018-04-30 09:55:18 +0200

    rhine: bring shims in line with other sony devices
    Change-Id: Ic4435dd9a225dcc01ff73a0d9ec7078213249ea0

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -68,9 +68,10 @@ TARGET_LD_SHIM_LIBS := \
 	/system/lib/hw/camera.vendor.qcom.so|/system/vendor/lib/libshim_camera.so \
 	/system/lib/hw/camera.vendor.qcom.so|/system/vendor/lib/libshim_cald.so \
 	/system/lib/hw/camera.vendor.qcom.so|libsensor.so \
+	/system/lib/libcald_pal.so|/system/vendor/lib/libshim_cald.so \
 	/system/lib/libcammw.so|/system/vendor/lib/libshim_cald.so \
 	/system/lib/libcammw.so|libsensor.so \
-	/system/lib/libsomc_chokoballpal.so|/system/vendor/lib/libGraphicBuffer.so \
+	/system/lib/libsomc_chokoballpal.so|/system/vendor/lib/libshim_camera.so \
 	/system/vendor/bin/mm-qcamera-daemon|/system/vendor/lib/libc_util.so \
 	/system/vendor/bin/mm-qcamera-daemon|libandroid.so
 
