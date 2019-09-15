1083ac2dfeca7f9e21e6cf68e6d2b05781b7a0b5
SpiritCroc <dev@spiritcroc.de>
2019-09-15 16:43:00 +0200

    Disable widevine
    Change-Id: Iaedc2736e5e865ba5489ff9ff177de488d14d6ec

--- rhine.mk
+++ rhine.mk
@@ -202,4 +202,4 @@ PRODUCT_COPY_FILES += \
 
 # Include non-opensource parts
 $(call inherit-product, vendor/sony/rhine-common/rhine-common-vendor.mk)
-$(call inherit-product, vendor/sony/widevine/widevine.mk)
+#$(call inherit-product, vendor/sony/widevine/widevine.mk)
